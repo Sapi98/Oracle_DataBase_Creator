06b253de13003e4cacec19b85bd88c3805fbca72
sapi <saptarshi.biswas9@gmail.com>
2019-03-17 01:10:49 +0530

    Adding capital and small charectar bank and creating a method gen_string() to generate a string.

--- oracleDatabaseConnection_Entry.py
+++ oracleDatabaseConnection_Entry.py
@@ -5,21 +5,41 @@ import datetime as dt
 
 class Essentials:
 
+    cap_char_bank = [chr(i) for i in range(65, 91)]
+    small_char_bank = [chr(i) for i in range(97, 123)]
+
     def __init__(self):
+        chances = 3
         self.cursor = None
         self.conn = None
+
+        while chances >= 0:
+            try:
+                self.initialize()
+                break
+            except:
+                print('!!!You have entered wrong Username or Password!!!')
+                chances -= 1
+                if chances == 0:
+                    exit(0)
+                else:
+                    print('You have {0} tries left'.format(chances))
+
+        self.initialize()
         self.tableName = input('Enter the table name : ')
         self.attrib = dict()
         c = int(input('Enter the number of Columns present in the table : '))
         for i in range(c):
             col = input('Enter Column Name ' + str(i + 1) + ' : ')
             dtype = input('Enter the Data Type : ')
+
             self.attrib[col] = dtype
         self.n = int(input('Enter the no of entries you want to enter : '))
         self.months = [
             'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
             'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
         ]
+
 
     def initialize(self):
         username = input('Enter the Username : ').strip()
@@ -35,6 +55,12 @@ class Essentials:
         date = str(day)+'-'+month+'-'+str(year)
         return date
 
+    def gen_string(self, length):
+        name = r.choice(self.cap_char_bank)
+        length -= 1
+        for i in range(length):
+            name = name + r.choice(self.small_char_bank)
+        return name
 
 if __name__ == '__main__':
     e = Essentials()
