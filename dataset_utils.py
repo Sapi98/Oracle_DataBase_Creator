9f23580c8a49c91e8616e95d0d6b2d6bbd9fd540
sapi <saptarshi.biswas9@gmail.com>
2019-03-31 02:31:51 +0530

    Path creation. Continue generate_data_list() function for name_length == 3 and general first_names and last_names.

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,6 +1,22 @@
 from openpyxl import load_workbook
+import os
 
 
-path1 = ''
-path2 = ''
-path3 = ''
+class Create:
+    def __init__(self):
+        cd = os.getcwd()
+        self.path1 = cd + '/Indian_first_names.xlsx'
+        self.path2 = cd + '/Indian_middle_names.xlsx'
+        self.path3 = cd + '/Indian_last_names.xlsx'
+
+    def generate_data_list(self, name_length = 2):
+        wb1 = load_workbook(filename=self.path1)
+        wb2 = None
+        wb3 = load_workbook(filename=self.path3)
+
+        first_names = []
+        last_names = []
+        middle_names = []
+
+        if name_length == 3:
+            wb2 = load_workbook(filename=self.path2)
